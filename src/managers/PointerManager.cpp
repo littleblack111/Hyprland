7d12bb906aae9ee7c7a33ef586b7c74b08e4d27c
Gwilherm Folliot <gwilherm55fo@gmail.com>
2024-10-22 11:54:16 +0200

    input-capture: fix rebase issues

--- src/managers/PointerManager.cpp
+++ src/managers/PointerManager.cpp
@@ -942,14 +942,7 @@ void CPointerManager::attachPointer(SP<IPointer> pointer) {
     });
 
     listener->frame = pointer->m_pointerEvents.frame.registerListener([] (std::any e) {
-        bool shouldSkip = false;
-        if (!g_pSeatManager->mouse.expired() && g_pInputManager->isLocked()) {
-            auto PMONITOR = g_pCompositor->m_lastMonitor.get();
-            shouldSkip = PMONITOR && PMONITOR->shouldSkipScheduleFrameOnMouseEvent();
-        }
-        g_pSeatManager->isPointerFrameSkipped = shouldSkip;
-        if (!g_pSeatManager->isPointerFrameSkipped)
-            g_pSeatManager->sendPointerFrame();
+        g_pInputManager->onMouseFrame();
     });
 
     listener->swipeBegin = pointer->m_pointerEvents.swipeBegin.registerListener([] (std::any e) {
